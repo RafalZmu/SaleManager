66861e00869bc1698da699db95820062eb167267
Rafał Żmuda <36961066+RafalZmu@users.noreply.github.com>
2023-12-13 14:34:01 +0100

    added items ordering by id

--- SaleManeger/ViewModels/ClientEditionViewModel.cs
+++ SaleManeger/ViewModels/ClientEditionViewModel.cs
@@ -12,239 +12,240 @@ using System.Reactive.Linq;
 
 namespace SaleManeger.ViewModels
 {
-    public class ClientEditionViewModel : ViewModelBase, INotifyPropertyChanged
-    {
-        #region Fields
+	public class ClientEditionViewModel : ViewModelBase, INotifyPropertyChanged
+	{
+		#region Fields
 
-        private string _order;
-        private List<Product> _products;
-        private string _sale;
-        private string _saleID;
-        private string _saleSum;
+		private string _order;
+		private List<Product> _products;
+		private string _sale;
+		private string _saleID;
+		private string _saleSum;
 
-        #endregion Fields
+		#endregion Fields
 
-        #region Properties
+		#region Properties
 
-        public Client Client { get; set; }
-        public string ClientID { get; set; }
-        public string Codes { get; set; }
-        public string Name { get; set; }
-        public string Number { get; set; }
-        public ReactiveCommand<Unit, string> OpenClientSelectionCommand { get; }
+		public Client Client { get; set; }
+		public string ClientID { get; set; }
+		public string Codes { get; set; }
+		public string Name { get; set; }
+		public string Number { get; set; }
+		public ReactiveCommand<Unit, string> OpenClientSelectionCommand { get; }
 
-        public string Order
-        {
-            get => _order;
-            set
-            {
-                if (_order != value)
-                {
-                    this.RaiseAndSetIfChanged(ref _order, value, nameof(Order));
-                }
-            }
-        }
+		public string Order
+		{
+			get => _order;
+			set
+			{
+				if (_order != value)
+				{
+					this.RaiseAndSetIfChanged(ref _order, value, nameof(Order));
+				}
+			}
+		}
 
-        public string Sale
-        {
-            get => _sale;
-            set
-            {
-                _sale = value;
-                UpdateSaleSum();
-            }
-        }
+		public string Sale
+		{
+			get => _sale;
+			set
+			{
+				_sale = value;
+				UpdateSaleSum();
+			}
+		}
 
-        public string SaleSum
-        {
-            get => _saleSum;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _saleSum, value);
-            }
-        }
+		public string SaleSum
+		{
+			get => _saleSum;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _saleSum, value);
+			}
+		}
 
-        private IProjectRepository _dataBase { get; set; }
+		private IProjectRepository _dataBase { get; set; }
 
-        #endregion Properties
+		#endregion Properties
 
-        #region Public Constructors
+		#region Public Constructors
 
-        public ClientEditionViewModel(IProjectRepository db, Client client, string saleID)
-        {
-            _dataBase = db;
-            _saleID = saleID;
-            Name = client.Name;
-            Number = client.PhoneNumber;
-            ClientID = client.ID;
-            Client = client;
+		public ClientEditionViewModel(IProjectRepository db, Client client, string saleID)
+		{
+			_dataBase = db;
+			_saleID = saleID;
+			Name = client.Name;
+			Number = client.PhoneNumber;
+			ClientID = client.ID;
+			Client = client;
 
-            OpenClientSelectionCommand = ReactiveCommand.Create(OpenClientSelection);
+			OpenClientSelectionCommand = ReactiveCommand.Create(OpenClientSelection);
 
-            _products = _dataBase.GetAll<Product>().AsNoTracking().ToList();
-            _products.ForEach(p =>
-            {
-                Codes += $"{p.Code}-{p.Name}{Environment.NewLine}";
-            });
+			_products = _dataBase.GetAll<Product>().AsNoTracking().ToList();
+			_products = _products.OrderBy(x => x.Code).ToList();
+			_products.ForEach(p =>
+			{
+				Codes += $"{p.Code}-{p.Name}{Environment.NewLine}";
+			});
 
-            foreach (var item in client.Products)
-            {
-                if (item.IsReserved)
-                {
-                    Order += $"{item.Name}{(string.IsNullOrEmpty(item.Name) ? "" : ": ")}{item.Value}{Environment.NewLine}";
-                }
-                else
-                {
-                    Sale += $"{item.Name}{(string.IsNullOrEmpty(item.Name) ? "" : ": ")}{item.Value}{Environment.NewLine}";
-                }
-            }
-        }
+			foreach (var item in client.Products)
+			{
+				if (item.IsReserved)
+				{
+					Order += $"{item.Name}{(string.IsNullOrEmpty(item.Name) ? "" : ": ")}{item.Value}{Environment.NewLine}";
+				}
+				else
+				{
+					Sale += $"{item.Name}{(string.IsNullOrEmpty(item.Name) ? "" : ": ")}{item.Value}{Environment.NewLine}";
+				}
+			}
+		}
 
-        #endregion Public Constructors
+		#endregion Public Constructors
 
-        #region Public Methods
+		#region Public Methods
 
-        public static void SaveClientOrder(IProjectRepository dataBase, Client client, string saleID)
-        {
-            //Delete old client orders
-            foreach (var item in dataBase.GetAll<ClientOrder>().Where(x => x.ClientID == client.ID && x.SaleID == saleID))
-            {
-                dataBase.Delete(item);
-            }
-            dataBase.Save();
+		public static void SaveClientOrder(IProjectRepository dataBase, Client client, string saleID)
+		{
+			//Delete old client orders
+			foreach (var item in dataBase.GetAll<ClientOrder>().Where(x => x.ClientID == client.ID && x.SaleID == saleID))
+			{
+				dataBase.Delete(item);
+			}
+			dataBase.Save();
 
-            //Add new client orders
-            foreach (var order in client.Products)
-            {
-                if (order.ID == "")
-                {
-                    order.ID = "Comment";
-                }
-                dataBase.Add(new ClientOrder()
-                {
-                    ClientID = client.ID,
-                    ClientOrderID = Guid.NewGuid().ToString(),
-                    ProductID = order.ID,
-                    SaleID = saleID,
-                    Date = DateTime.Now,
-                    Value = order.Value,
-                    IsReserved = order.IsReserved
-                });
-                dataBase.Save();
-            }
-        }
+			//Add new client orders
+			foreach (var order in client.Products)
+			{
+				if (order.ID == "")
+				{
+					order.ID = "Comment";
+				}
+				dataBase.Add(new ClientOrder()
+				{
+					ClientID = client.ID,
+					ClientOrderID = Guid.NewGuid().ToString(),
+					ProductID = order.ID,
+					SaleID = saleID,
+					Date = DateTime.Now,
+					Value = order.Value,
+					IsReserved = order.IsReserved
+				});
+				dataBase.Save();
+			}
+		}
 
-        public List<Product> GetProductsFromText(List<Product> products, string text, bool IsReserved)
-        {
-            var productsList = new List<Product>();
-            if (string.IsNullOrWhiteSpace(text))
-                return productsList;
+		public List<Product> GetProductsFromText(List<Product> products, string text, bool IsReserved)
+		{
+			var productsList = new List<Product>();
+			if (string.IsNullOrWhiteSpace(text))
+				return productsList;
 
-            foreach (var item in text.Trim().Split("\n"))
-            {
-                if (!item.Contains(':'))
-                {
-                    Product comment = new()
-                    {
-                        ID = "",
-                        Name = "",
-                        Code = "",
-                        Value = item.Trim(),
-                        IsReserved = IsReserved
-                    };
-                    productsList.Add(comment);
-                    continue;
-                }
-                var name = item.Split(":")[0].Trim();
-                var value = item.Split(':')[1].Trim();
-                var code = products.First(x => x.Name == name).Code;
-                var ID = products.First(x => x.Code == code).ID;
-                Product product = new()
-                {
-                    ID = ID,
-                    Name = name,
-                    Code = code,
-                    Value = value,
-                    IsReserved = IsReserved
-                };
-                productsList.Add(product);
-            }
-            return productsList;
-        }
+			foreach (var item in text.Trim().Split("\n"))
+			{
+				if (!item.Contains(':'))
+				{
+					Product comment = new()
+					{
+						ID = "",
+						Name = "",
+						Code = "",
+						Value = item.Trim(),
+						IsReserved = IsReserved
+					};
+					productsList.Add(comment);
+					continue;
+				}
+				var name = item.Split(":")[0].Trim();
+				var value = item.Split(':')[1].Trim();
+				var code = products.First(x => x.Name == name).Code;
+				var ID = products.First(x => x.Code == code).ID;
+				Product product = new()
+				{
+					ID = ID,
+					Name = name,
+					Code = code,
+					Value = value,
+					IsReserved = IsReserved
+				};
+				productsList.Add(product);
+			}
+			return productsList;
+		}
 
-        #endregion Public Methods
+		#endregion Public Methods
 
-        #region Private Methods
+		#region Private Methods
 
-        private string OpenClientSelection()
-        {
-            SaveClient();
-            return _saleID;
-        }
+		private string OpenClientSelection()
+		{
+			SaveClient();
+			return _saleID;
+		}
 
-        private void SaveClient()
-        {
-            Client.Products.Clear();
-            // Return if all fields are empty
-            if (string.IsNullOrWhiteSpace(Name) && string.IsNullOrWhiteSpace(Number) && string.IsNullOrWhiteSpace(Sale) && string.IsNullOrWhiteSpace(Order))
-            {
-                return;
-            }
-            Client.Name = string.IsNullOrWhiteSpace(Name) ? "" : Name;
-            Client.PhoneNumber = string.IsNullOrWhiteSpace(Number) ? "" : Number;
+		private void SaveClient()
+		{
+			Client.Products.Clear();
+			// Return if all fields are empty
+			if (string.IsNullOrWhiteSpace(Name) && string.IsNullOrWhiteSpace(Number) && string.IsNullOrWhiteSpace(Sale) && string.IsNullOrWhiteSpace(Order))
+			{
+				return;
+			}
+			Client.Name = string.IsNullOrWhiteSpace(Name) ? "" : Name;
+			Client.PhoneNumber = string.IsNullOrWhiteSpace(Number) ? "" : Number;
 
-            //Get ordered products
-            Client.Products.AddRange(GetProductsFromText(_products, Order, true));
+			//Get ordered products
+			Client.Products.AddRange(GetProductsFromText(_products, Order, true));
 
-            //Get sold products
-            Client.Products.AddRange(GetProductsFromText(_products, Sale, false));
+			//Get sold products
+			Client.Products.AddRange(GetProductsFromText(_products, Sale, false));
 
-            //Save client
-            if (!_dataBase.GetAll<Client>().AsNoTracking().Any(x => x.ID == Client.ID))
-                _dataBase.Add(Client);
-            else
-                _dataBase.Update(Client);
+			//Save client
+			if (!_dataBase.GetAll<Client>().AsNoTracking().Any(x => x.ID == Client.ID))
+				_dataBase.Add(Client);
+			else
+				_dataBase.Update(Client);
 
-            SaveClientOrder(_dataBase, Client, _saleID);
-        }
+			SaveClientOrder(_dataBase, Client, _saleID);
+		}
 
-        private void UpdateSaleSum()
-        {
-            if (string.IsNullOrWhiteSpace(Sale))
-                return;
+		private void UpdateSaleSum()
+		{
+			if (string.IsNullOrWhiteSpace(Sale))
+				return;
 
-            SaleSum = "";
-            List<string> sumOfOrderSections = new();
-            double sum = 0;
-            // Get sum of each order
-            foreach (var line in Sale.Split("\n"))
-            {
-                // If line contains ':' it means that it is a product
-                if (line.Contains(':'))
-                {
-                    double.TryParse(line.Split(":")[1].Trim().Replace(",", ".").Split(" ")[0], out double productCost);
-                    sum += productCost;
-                }
-                else
-                {
-                    sumOfOrderSections.Add(sum.ToString());
-                    sum = 0;
-                }
-            }
-            SaleSum = string.Join(" + ", sumOfOrderSections);
+			SaleSum = "";
+			List<string> sumOfOrderSections = new();
+			double sum = 0;
+			// Get sum of each order
+			foreach (var line in Sale.Split("\n"))
+			{
+				// If line contains ':' it means that it is a product
+				if (line.Contains(':'))
+				{
+					double.TryParse(line.Split(":")[1].Trim().Replace(",", ".").Split(" ")[0], out double productCost);
+					sum += productCost;
+				}
+				else
+				{
+					sumOfOrderSections.Add(sum.ToString());
+					sum = 0;
+				}
+			}
+			SaleSum = string.Join(" + ", sumOfOrderSections);
 
-            // Get sum of all orders
-            double allSaleSum = SaleSum.Split("+").Sum(x => double.Parse(x));
-            SaleSum += $"{Environment.NewLine}Suma: {allSaleSum}";
+			// Get sum of all orders
+			double allSaleSum = SaleSum.Split("+").Sum(x => double.Parse(x));
+			SaleSum += $"{Environment.NewLine}Suma: {allSaleSum}";
 
-            if (allSaleSum != 0)
-            {
-                // Get change
-                SaleSum += $"       Reszta z {Math.Ceiling(allSaleSum / 100) * 100}: {Math.Ceiling(allSaleSum / 100) * 100 - allSaleSum}";
-                SaleSum += $"  Reszta z {Math.Ceiling(allSaleSum / 50) * 50}: {Math.Ceiling(allSaleSum / 50) * 50 - allSaleSum}";
-            }
-        }
+			if (allSaleSum != 0)
+			{
+				// Get change
+				SaleSum += $"       Reszta z {Math.Ceiling(allSaleSum / 100) * 100}: {Math.Ceiling(allSaleSum / 100) * 100 - allSaleSum}";
+				SaleSum += $"  Reszta z {Math.Ceiling(allSaleSum / 50) * 50}: {Math.Ceiling(allSaleSum / 50) * 50 - allSaleSum}";
+			}
+		}
 
-        #endregion Private Methods
-    }
+		#endregion Private Methods
+	}
 }
--- SaleManeger/ViewModels/SaleSummaryViewModel.cs
+++ SaleManeger/ViewModels/SaleSummaryViewModel.cs
@@ -4,106 +4,102 @@ using SaleManeger.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reactive;
 
 namespace SaleManeger.ViewModels
 {
-    public class SaleSummaryViewModel : ViewModelBase
-    {
-        #region Private Fields
+	public class SaleSummaryViewModel : ViewModelBase
+	{
+		private List<Client> _clients;
+		private List<Product> _products;
 
-        private List<Client> _clients;
-        private List<Product> _products;
+		#region Public Constructors
 
-        #endregion Private Fields
+		public SaleSummaryViewModel(IProjectRepository dataBase, string saleName)
+		{
+			_dataBase = dataBase;
+			SaleName = saleName;
 
-        #region Public Constructors
+			// Get all clients and their orders.
+			var ProductsList = _dataBase.GetAll<Product>().ToList();
+			var clients = _dataBase.GetAll<Client>().ToList();
+			var clientsOrders = _dataBase.GetAll<ClientOrder>().Where(x => x.SaleID == SaleName).ToList();
+			var clientsOrdersList = clientsOrders.GroupBy(x => x.ClientID).Select(y => y.ToList()).ToList();
+			foreach (var clientOrder in clientsOrdersList)
+			{
+				ObservableCollection<Product> orders = new();
+				foreach (var order in clientOrder)
+				{
+					if (order.ProductID == "Comment")
+						continue;
+					Product product = ProductsList.FirstOrDefault(x => order.ProductID == x.ID);
 
-        public SaleSummaryViewModel(IProjectRepository dataBase, string saleName)
-        {
-            _dataBase = dataBase;
-            SaleName = saleName;
+					orders.Add(new Product()
+					{
+						Name = product.Name,
+						ID = product.ID,
+						IsReserved = order.IsReserved,
+						PricePerKg = product.PricePerKg,
+						Value = order.Value,
+						Code = product.Code,
+					});
+				}
+				clients.Where(x => x.ID == clientOrder[0].ClientID).FirstOrDefault().Products = orders;
+			}
+			clients.ForEach(x => x.Products ??= new ObservableCollection<Product>());
+			_clients = clients;
 
-            // Get all clients and their orders.
-            var ProductsList = _dataBase.GetAll<Product>().ToList();
-            var clients = _dataBase.GetAll<Client>().ToList();
-            var clientsOrders = _dataBase.GetAll<ClientOrder>().Where(x => x.SaleID == SaleName).ToList();
-            var clientsOrdersList = clientsOrders.GroupBy(x => x.ClientID).Select(y => y.ToList()).ToList();
-            foreach (var clientOrder in clientsOrdersList)
-            {
-                ObservableCollection<Product> orders = new();
-                foreach (var order in clientOrder)
-                {
-                    if (order.ProductID == "Comment")
-                        continue;
-                    Product product = ProductsList.FirstOrDefault(x => order.ProductID == x.ID);
+			_products = _dataBase.GetAll<Product>().ToList();
+			_products = _products.OrderBy(x => x.Code).ToList();
+			ClientsLeft = _clients.Count(x => (x.Products.Any(y => (y.IsReserved == true))) && (!x.Products.Any(y => y.IsReserved == false)));
+			GetProducts();
 
-                    orders.Add(new Product()
-                    {
-                        Name = product.Name,
-                        ID = product.ID,
-                        IsReserved = order.IsReserved,
-                        PricePerKg = product.PricePerKg,
-                        Value = order.Value,
-                        Code = product.Code,
-                    });
-                }
-                clients.Where(x => x.ID == clientOrder[0].ClientID).FirstOrDefault().Products = orders;
-            }
-            clients.ForEach(x => x.Products ??= new ObservableCollection<Product>());
-            _clients = clients;
+			OpenClientSelectionCommand = ReactiveCommand.Create(() => { return SaleName; });
+		}
 
-            _products = _dataBase.GetAll<Product>().ToList();
-            ClientsLeft = _clients.Count(x => (x.Products.Any(y => (y.IsReserved == true))) && (!x.Products.Any(y => y.IsReserved == false)));
-            GetProducts();
+		#endregion Public Constructors
 
-            OpenClientSelectionCommand = ReactiveCommand.Create(() => { return SaleName; });
-        }
+		public IProjectRepository _dataBase { get; set; }
+		public string AllOrders { get; set; }
+		public int ClientsLeft { get; set; }
+		public ReactiveCommand<Unit, string> OpenClientSelectionCommand { get; set; }
+		public string OrdersLeft { get; set; }
+		public string SaleName { get; set; }
+		public string SoldAll { get; set; }
 
-        #endregion Public Constructors
+		#region Private Methods
 
-        #region Public Properties
+		private void GetProducts()
+		{
+			CultureInfo culture = new CultureInfo("en-US");
+			// Get all orders, orders left and all sold products
+			foreach (var product in _products)
+			{
+				AllOrders += $"{product.Name}: {_dataBase.GetAll<ClientOrder>()
+					.Where(x => x.ProductID == product.ID && x.IsReserved == true && x.SaleID == SaleName)
+					.ToList()
+					.Sum(x => double.Parse(x.Value.Split(' ')[0], culture))}{Environment.NewLine}";
 
-        public IProjectRepository _dataBase { get; set; }
-        public string AllOrders { get; set; }
-        public int ClientsLeft { get; set; }
-        public ReactiveCommand<Unit, string> OpenClientSelectionCommand { get; set; }
-        public string OrdersLeft { get; set; }
-        public string SaleName { get; set; }
-        public string SoldAll { get; set; }
+				var sum = 0.0;
+				foreach (var client in _clients)
+				{
+					if (client.Products.Any(x => x.IsReserved == false))
+						continue;
 
-        #endregion Public Properties
+					sum += client.Products.Where(x => x.ID == product.ID).Sum(x => double.Parse(x.Value.Split(" ")[0], culture));
+				}
+				OrdersLeft += $"{product.Name}: {sum}{Environment.NewLine}";
 
-        #region Private Methods
+				SoldAll += $"{product.Name}: {_dataBase.GetAll<ClientOrder>()
+					.Where(x => x.ProductID == product.ID && x.IsReserved == false && x.SaleID == SaleName)
+					.ToList()
+					.Sum(x => double.Parse(x.Value.Split(' ')[0], culture))}{Environment.NewLine}";
+			}
+		}
 
-        private void GetProducts()
-        {
-            // Get all orders, orders left and all sold products
-            foreach (var product in _products)
-            {
-                AllOrders += $"{product.Name}: {_dataBase.GetAll<ClientOrder>()
-                    .Where(x => x.ProductID == product.ID && x.IsReserved == true && x.SaleID == SaleName)
-                    .ToList()
-                    .Sum(x => double.Parse(x.Value.Split(' ')[0]))}{Environment.NewLine}";
-
-                var sum = 0.0;
-                foreach (var client in _clients)
-                {
-                    if (client.Products.Any(x => x.IsReserved == false))
-                        continue;
-
-                    sum += client.Products.Where(x => x.ID == product.ID).Sum(x => double.Parse(x.Value.Split(" ")[0]));
-                }
-                OrdersLeft += $"{product.Name}: {sum}{Environment.NewLine}";
-
-                SoldAll += $"{product.Name}: {_dataBase.GetAll<ClientOrder>()
-                    .Where(x => x.ProductID == product.ID && x.IsReserved == false && x.SaleID == SaleName)
-                    .ToList()
-                    .Sum(x => double.Parse(x.Value.Split(' ')[0]))}{Environment.NewLine}";
-            }
-        }
-
-        #endregion Private Methods
-    }
+		#endregion Private Methods
+	}
 }
